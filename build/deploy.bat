25f96e85f56b911c67ac73f258c4c56e68deb750
Demis Bellot <demis.bellot@gmail.com>
2011-06-27 03:02:23 +0100

    updated build script

--- build/deploy.bat
+++ build/deploy.bat
@@ -1,7 +1,7 @@
 COPY ..\src\ServiceStack.Logging.EventLog\bin\Release\ServiceStack.Logging.EventLog.* release\ServiceStack.Logging\EventLog
 COPY ..\src\ServiceStack.Logging.Log4Net\bin\Release\ServiceStack.Logging.Log4Net.* release\ServiceStack.Logging\Log4Net
 COPY ..\src\ServiceStack.Logging.Log4Netv129\bin\Release\ServiceStack.Logging.Log4Netv129.* release\ServiceStack.Logging\Log4Netv129
-COPY ..\lib\log4net.1.2.9.dll release\ServiceStack.Logging\Log4Netv129
-COPY ..\lib\log4net.dll release\ServiceStack.Logging\Log4Net
-COPY ..\lib\ServiceStack.Interfaces.dll release\ServiceStack.Logging
+COPY ..\lib\log4net.1.2.9.dll release\ServiceStack.Logging\Log4Netv129\
+COPY ..\lib\log4net.dll release\ServiceStack.Logging\Log4Net\
+COPY ..\lib\ServiceStack.Interfaces.dll release\ServiceStack.Logging\
 
