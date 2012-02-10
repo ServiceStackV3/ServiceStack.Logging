adb442cec425808db9d2c7df33cf57d1434389d8
Ethan J. Brown <ethan.j.brown@gmail.com>
2012-02-10 10:12:05 -0500

    don't need this stuff anymore

--- build/deploy.bat
+++
@@ -1,9 +0,0 @@
-COPY ..\src\ServiceStack.Logging.EventLog\bin\Release\ServiceStack.Logging.EventLog.* release\ServiceStack.Logging\EventLog
-COPY ..\src\ServiceStack.Logging.Log4Net\bin\Release\ServiceStack.Logging.Log4Net.* release\ServiceStack.Logging\Log4Net
-COPY ..\src\ServiceStack.Logging.Log4Netv129\bin\Release\ServiceStack.Logging.Log4Netv129.* release\ServiceStack.Logging\Log4Netv129
-COPY ..\src\ServiceStack.Logging.EventLog\bin\Release\ServiceStack.Logging.Elmah.* release\ServiceStack.Logging\Elmah
-COPY ..\lib\log4net.1.2.9.dll release\ServiceStack.Logging\Log4Netv129\
-COPY ..\lib\log4net.dll release\ServiceStack.Logging\Log4Net\
-COPY ..\lib\Elmah.dll release\ServiceStack.Logging\Elmah\
-COPY ..\lib\ServiceStack.Interfaces.dll release\ServiceStack.Logging\
-
